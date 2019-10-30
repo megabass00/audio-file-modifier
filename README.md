27dfa10e8962881dc68fdf23fcadde4a454c4a3d
Luis Izquierdo <megabass00@hotmail.com>
2019-10-30 14:07:55 +0100

    Default folder to processed files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Audio File Normalize
-The purpose of this tool that runs in Node.js is to normalize the volume of the audio files that the user wishes to process.
+# Audio File Modifier
+
+The purpose of this tool that runs in Node.js is to do several operations (normalize, split silences, apply a effect...) on audio files that the user wishes to process.
+
+# Operations
+
+- **normalize**: You can normalize any local sound file wich you spcify.
+- **volume**: You can change volume on any local file wich you specify.
+- **split**: You can remove the initial and final blank parts.
+- **fade**: You can apply fade in/out/both to any local sound file.
+- **pitch**: You can change pitch on any local audio file.
+- **play**: (Improvement) You can play any song from your local disk.
+- **record**: (Improvement) You can record mic built-in signal and save in a mp3 file. You need to have installed on your machine sox (Mac/Windows Users) or ALSA tools for Linux.
+- **effect**: (Improvement) You can apply an effect to an audio file between several types.
--- index.js
+++ index.js
@@ -7,61 +7,148 @@ const getParam = (param, defaultVal = '') => {
   return paramValue;
 };
 
-const checkOption = (param, defaultVal = false) => {
-  let paramValue = defaultVal;
-  process.argv.forEach(val => {
-    if (val.indexOf(param) > -1) paramValue = true;
-  });
-  return paramValue;
-};
+const AudioFileModifier = require('./AudioFileModifier');
 
-const AudioFileNormalize = require('./AudioFileNormalize');
+const outFile = getParam('outFile', 'example.mp3');
+const volume = getParam('volume', 1.0);
+const sampleRate = getParam('sampleRate', 44100);
+const channels = getParam('channels', 2);
+const pitch = getParam('pitch', 120);
+const fade = getParam('fade', false);
+const fadeIn = getParam('fadeIn', false);
+const fadeOut = getParam('fadeOut', false);
+const recTime = getParam('recTime', 10);
+const verbose = getParam('verbose', false);
 
-// const minRate = parseInt(getParam('rate', 320));
-// const minSimilarity = parseInt(getParam('similarity', 80)) / 100;
-// const engine = getParam('engine', 'myfreemp3');
-// const proxyAddress = getParam('proxy', false);
-// const noWindow = checkOption('no-window', false);
-// const minimizeWindow = checkOption('minimize', false);
-// const alwaysDownloadFiles = checkOption('alwaysDownloadFiles', true);
-
-const normalizer = new AudioFileNormalize();
+const audioModifier = new AudioFileModifier({
+  outFile,
+  volume,
+  sampleRate,
+  channels,
+  pitch,
+  fade,
+  fadeIn,
+  fadeOut,
+  recTime,
+  verbose,
+});
 const args = process.argv.slice(2);
 
 switch (args[0]) {
   case 'normalize':
-    // if (!args[1]) {
-    //   normalizer.log('Error:'.bgRed, 'You must enter a valid local path file'.red);
-    //   return;
-    // }
-    normalizer.normalizeFile(args[1]);
+    if (!args[1]) {
+      audioModifier.log('Error:'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    audioModifier.normalizeFile(args[1]);
     break;
 
-  // case 'song':
-  //   if (!args[1]) {
-  //     normalizer.log('Error:'.bgRed, 'You must enter a song title wrapped between quotes'.red);
-  //     return;
-  //   }
-  //   normalizer.dowmloadSong(args[1]);
-  //   break;
+  case 'volume':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    if (!args[2]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter your output volume wished'.red);
+      return;
+    }
+    audioModifier.changeFileVolume(args[1], args[2]);
+    break;
 
-  // case 'export':
-  //   if (!args[1]) {
-  //     normalizer.log('Error:'.bgRed, 'You must enter a valid playlist ID for export'.red);
-  //     return;
-  //   }
-  //   normalizer.exportPlaylist(args[1]);
-  //   break;
+  case 'split':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    audioModifier.splitSilences(args[1]);
+    break;
 
-  // case 'download':
-  //   if (!args[1]) {
-  //     normalizer.log('Error:'.bgRed, 'You must enter a valid file path'.red);
-  //     return;
-  //   }
-  //   normalizer.importAndDownloadFile(args[1]);
-  //   break;
+  case 'pitch':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    if (!args[2]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter wished pitch percent to change'.red);
+      return;
+    }
+    if (parseInt(args[2]) < 1 || parseInt(args[2]) > 200) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'Pitch percent to change must be between 1 and 200'.red);
+      return;
+    }
+    audioModifier.changePitch(args[1], parseInt(args[2]));
+    break;
+
+  case 'fade':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    if (!args[2]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter wished time to fade in/out'.red);
+      return;
+    }
+    if (parseInt(args[2]) < 1 || parseInt(args[2]) > 30) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'Fade time must be between 1 and 30 seconds'.red);
+      return;
+    }
+    audioModifier.createFade(args[1], parseInt(args[2]));
+    break;
+
+  case 'record':
+    if (!args[1] || parseInt(args[1]) < 1) {
+      audioModifier.error(
+        'Invalid Parameter'.bgRed,
+        'You must enter a valid seconds time to record mic (minimun 3 sec)'.red,
+      );
+      return;
+    }
+    audioModifier.recordMic(parseInt(args[1]));
+    break;
+
+  case 'play':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    audioModifier.playAudioFile(args[1]);
+    break;
+
+  case 'effect':
+    if (!args[1]) {
+      audioModifier.error('Invalid Parameter'.bgRed, 'You must enter a valid local path file'.red);
+      return;
+    }
+    if (
+      !args[2] ||
+      ![
+        'REVERB',
+        'ECHO',
+        'CHORUS',
+        'FLANGER',
+        'DISTORT',
+        'LOWPASS',
+        'HIGHPASS',
+        'BENDUP',
+        'BENDDOWN',
+        'REVERSE',
+      ].includes(args[2].toUpperCase())
+    ) {
+      audioModifier.error(
+        'Invalid Parameter'.bgRed,
+        'You must enter a valid effect:'.red +
+          '(reverb | echo | chorus | flanger | distort | lowpass | highpass | bendup | benddown | reverse)'.bgYellow
+            .black,
+      );
+      return;
+    }
+    audioModifier.applyEffect(args[1], args[2]);
+    break;
 
   default:
-    downloader.log('Error'.bgRed, 'Sorry, enter a valid operation'.red);
-    downloader.log('Valid operations:'.yellow, '(playlist | song | export | download)'.cyan);
+    audioModifier.error('Error'.bgRed, 'Sorry, enter a valid operation'.red);
+    audioModifier.error(
+      'Valid operations:'.bgYellow.red,
+      '(normalize | volume | split | pitch | fade | record | play | effect)'.bgYellow.black,
+    );
 }
